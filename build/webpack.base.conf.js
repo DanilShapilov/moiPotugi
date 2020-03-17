528ce51ec6dc564032a04b49b40138131893e9a2
DanilShapilov <danil.shapilov225@gmail.com>
2020-03-17 20:57:33 +0500

    pug installed

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,7 +6,12 @@ const fs = require("fs");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { VueLoaderPlugin } = require("vue-loader");
+// const { VueLoaderPlugin } = require("vue-loader");
+
+// new webpack.ProvidePlugin({
+//   $: 'jquery',
+//   jQuery: 'jquery'
+// })
 
 // Main const. Feel free to change it
 const PATHS = {
@@ -17,10 +22,11 @@ const PATHS = {
 
 // Pages const for HtmlWebpackPlugin
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
-const PAGES_DIR = PATHS.src;
+// const PAGES_DIR = PATHS.src;
+const PAGES_DIR = `${PATHS.src}/pug/pages`;
 const PAGES = fs
   .readdirSync(PAGES_DIR)
-  .filter(fileName => fileName.endsWith(".html"));
+  .filter(fileName => fileName.endsWith(".pug"));
 
 module.exports = {
   externals: {
@@ -50,21 +56,26 @@ module.exports = {
   module: {
     rules: [
       {
+        // Pug
+        test: /\.pug$/,
+        loader: "pug-loader"
+      },
+      {
         // JavaScript
         test: /\.js$/,
         loader: "babel-loader",
         exclude: "/node_modules/"
       },
-      {
-        // Vue
-        test: /\.vue$/,
-        loader: "vue-loader",
-        options: {
-          loader: {
-            scss: "vue-style-loader!css-loader!sass-loader"
-          }
-        }
-      },
+      // {
+      //   // Vue
+      //   test: /\.vue$/,
+      //   loader: "vue-loader",
+      //   options: {
+      //     loader: {
+      //       scss: "vue-style-loader!css-loader!sass-loader"
+      //     }
+      //   }
+      // },
       {
         // Fonts
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
@@ -128,11 +139,12 @@ module.exports = {
   resolve: {
     alias: {
       "~": PATHS.src,
-      vue$: "vue/dist/vue.js"
+      // vue$: "vue/dist/vue.js"
     }
   },
   plugins: [
-    new VueLoaderPlugin(),
+    // new VueLoaderPlugin(),
+    
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].[contenthash].css`
     }),
@@ -141,7 +153,6 @@ module.exports = {
       { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
       { from: `${PATHS.src}/static`, to: "" }
     ]),
-
     /*
       Automatic creation any html pages (Don't forget to RERUN dev server!)
       See more:
@@ -149,12 +160,9 @@ module.exports = {
       Best way to create pages:
       https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
     */
-    ...PAGES.map(
-      page =>
-        new HtmlWebpackPlugin({
-          template: `${PAGES_DIR}/${page}`,
-          filename: `./${page}`
-        })
-    )
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
   ]
 };
