3721530ca07ff2c603faa25687c87eb4efdb388d
DanilShapilov <danil.shapilov225@gmail.com>
2020-03-24 21:25:16 +0500

    dropdown refactored

--- src/pug/components/input-field/input-field.js
+++ src/pug/components/input-field/input-field.js
@@ -12,31 +12,25 @@ allDropdownEl.forEach(el => {
   }
   handleDropdownClick(dropdownInput)
   handleDropdownClick(dropdownArrow)
-  
+
   // + and - btns
   // find all buttons and handle click
   dropdownOptions.querySelectorAll('.input-field_dropdown-block-btn').forEach((btn) => {
     handleOptionClick(btn);
     // make minus btn disabled if option value is 0
-    if (btn.classList.contains('input-field_dropdown-block-btn--minus')) {
-      checkValueForZero(btn)
+    let dropdownOptionValue = +btn.parentElement.querySelector('.input-field_dropdown-amount-text').innerHTML;
+    
+    if (dropdownOptionValue === 0 && btn.classList.contains('input-field_dropdown-block-btn--minus')) {
+      btn.setAttribute('disabled','disabled');
     }
   })
-  // check if btn has class minus and if its h3 element
-  //  contains value of 0 make btn disabled
-  function checkValueForZero(btn) {
-    // find parent of a btn an then find value whitch is .input-field_dropdown-amount-text
-    let dropdownOptionValue = +btn.parentElement.querySelector('.input-field_dropdown-amount-text').innerText;
 
-    if(dropdownOptionValue === 0){
-        let minusBtn = btn.parentElement.querySelector('.input-field_dropdown-block-btn--minus')
-        minusBtn.setAttribute('disabled','disabled')
-    }
-    if (btn.classList.contains('input-field_dropdown-block-btn--plus')) {
-      let minusBtn = btn.parentElement.querySelector('.input-field_dropdown-block-btn--minus')
-      if (dropdownOptionValue > 0) {
-        minusBtn.removeAttribute('disabled')
-      }
+  function checkValueForZero(dropdownOptionValue,btn) {
+    let minusBtn = btn.parentElement.querySelector('.input-field_dropdown-block-btn--minus');
+    if(+dropdownOptionValue.innerText === 0){
+      minusBtn.setAttribute('disabled','disabled')
+    }else if (+dropdownOptionValue.innerText > 0) {
+      minusBtn.removeAttribute('disabled')
     }
   }
 
@@ -44,14 +38,16 @@ const dropdownInputText = dropdownInput.value;
 
   function handleOptionClick(btn) {
     let dropdownOptionValue = btn.parentElement.querySelector('.input-field_dropdown-amount-text');
+
     btn.addEventListener("click", () =>{
       if (btn.classList.contains('input-field_dropdown-block-btn--plus')) {
-        dropdownOptionValue.innerText = +dropdownOptionValue.innerText + 1; 
+        dropdownOptionValue.innerHTML = +dropdownOptionValue.innerHTML + 1; 
       }else if (btn.classList.contains('input-field_dropdown-block-btn--minus')) {
-        dropdownOptionValue.innerText = +dropdownOptionValue.innerText - 1; 
+        dropdownOptionValue.innerHTML = +dropdownOptionValue.innerHTML - 1; 
       }
-      checkValueForZero(btn);
-      changeDropdownInputValue(btn);
+      // make minus btn disabled of not depends on dropdownOptionValue
+      checkValueForZero(dropdownOptionValue, btn);
+      // changeDropdownInputValue(btn);
     })
   }
 
